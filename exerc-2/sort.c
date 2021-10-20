a901f14cce0166bf64e5e2b3f11777fdafca7ac3
tcfcoelho <tcfcoelho@usp.br>
2021-10-20 02:34:29 -0300

    exerc-2 (10.19)

--- exerc-2/sort.c
+++ exerc-2/sort.c
@@ -1,69 +1,87 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <glib.h>
-#include <string.h>
 
 typedef struct {
   char *data;
   int key;
 } item_t;
 
-gint compare_item(gconstpointer a, gconstpointer b) {
-  if (((item_t *)a)->key < ((item_t *)b)->key)
+/* item_t array[] = {
+  {"bill", 3},
+  {"neil", 4},
+  {"john", 2},
+  {"rick", 5},
+  {"alex", 1},
+}; */
+
+//comparação da ordem dos itens da lista
+gint compare(gconstpointer k1, gconstpointer k2){
+  if(((item_t*)k1)->key < ((item_t*)k2)->key){
     return -1;
-  else if (((item_t *)a)->key > ((item_t *)b)->key)
+  }
+
+  else if(((item_t*)k1)-> key > ((item_t*)k2)-> key){
     return 1;
+  }
+
   else
     return 0;
 }
 
-void print_item(gpointer data, gpointer user_data) {
-  int *i = (int *) user_data;
-  printf("No. %d -> key=%d: %s\n", *i + 1, ((item_t *)data)->key, ((item_t *)data)->data);
+//impressão dos itens da lista
+void print(gpointer data, gpointer user_data){
+  int *i = (int *)user_data;
+  printf("Nº %d -> key = %d: %s \n", *i +1, ((item_t*)data)-> key, ((item_t*)data)-> data);
+
   (*i)++;
 }
 
-item_t * item_init(char * data, int key) {
-  item_t * self = malloc(sizeof(item_t));
-  self->key = key;
-  self->data = strdup(data);
+
+//inicialização
+item_t *init(char *data, int key){
+  item_t *self = malloc(sizeof(item_t));
+  self -> key = key;
+  self -> data = strdup(data);
+
   return self;
 }
 
-void item_destroy(gpointer _self, gpointer unused_data) {
+//destrutor
+void destroy(gpointer _self, gpointer unused_data){
   item_t *self = (item_t *) _self;
   free(self->data);
   free(self);
 }
 
-char s1[40];
 
-int main(void) {
-  char *s = "bill";
-  GList *list = g_list_append(NULL, item_init(s, 3));
-  s = "neil";
-  list = g_list_insert_sorted(list, item_init(s, 4), compare_item);
-  strcpy(s1, "john");
-  list = g_list_insert_sorted(list, item_init(s1, 2), compare_item);
-  strcpy(s1, "sara");
-  list = g_list_insert_sorted(list, item_init(s1, 7), compare_item);
-  list = g_list_insert_sorted(list, item_init("rick", 5), compare_item);
-  list = g_list_insert_sorted(list, item_init("alex", 1), compare_item);
+int main (void){
 
-  /* printf("The first item is '%s'\n", ((item_t *)g_list_first(list)->data)->data); */
+  //criação da lista e adição do primeiro item:
+  GList *list = g_list_append(NULL, init("bill", 3));
+  
+  //adição de item por meio do ponteiro:
+  char *nome = "neil";
+  list = g_list_insert_sorted(list, init(nome, 4), compare); //necessário uso do compare para garantir ordenação
 
-  /* list = g_list_next(list); */
+  //reutilização do ponteiro para nova adição de item:
+  nome = "john";
+  list = g_list_insert_sorted(list, init(nome, 2), compare);
 
-  /* printf("The second item is '%s'\n", ((item_t *)list->data)->data); */
+  list = g_list_insert_sorted(list, init("rick", 5), compare);
+  list = g_list_insert_sorted(list, init("sara", 7), compare);
+  list = g_list_insert_sorted(list, init("alex", 1), compare);
+
 
   list = g_list_first(list);
 
   int i = 0;
-  g_list_foreach(list, print_item, &i);
+  g_list_foreach(list, print, &i); //exerce função do for(){print}
 
-  /* limpando toda a lista e suas estruturas */
+  
+  //desalocação:
   list = g_list_first(list);
-  g_list_foreach(list, item_destroy, NULL);
+  g_list_foreach(list, destroy, NULL);
   g_list_free(list);
 
   return 0;
